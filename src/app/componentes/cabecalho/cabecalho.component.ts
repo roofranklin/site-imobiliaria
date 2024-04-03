a6dbdc9581561e07f8dbc92feecfb0055e8e2ad3
Roosevelt Franklin <roosevelt_franklin@hotmail.com>
2024-04-03 16:08:34 -0300

    menu com links ok

--- src/app/componentes/cabecalho/cabecalho.component.ts
+++ src/app/componentes/cabecalho/cabecalho.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-cabecalho',
   standalone: true,
-  imports: [],
+  imports: [ RouterModule ],
   templateUrl: './cabecalho.component.html',
   styleUrl: './cabecalho.component.scss'
 })
